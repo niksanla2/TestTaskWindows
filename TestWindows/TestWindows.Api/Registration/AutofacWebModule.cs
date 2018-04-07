e971f5ed42d32fb3aba59e99155fbd50d37b7690
Nikita Del <niksan_la2@mail.ru>
2018-04-07 18:34:26 +0300

    add core asp net. Add Autofac

---
+++ TestWindows/TestWindows.Api/Registration/AutofacWebModule.cs
@@ -0,0 +1,12 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestWindows.Api.Registration
+{
+    public class AutofacWebModule : Module
+    {
+    }
+}
---
+++ TestWindows/TestWindows.Api/Startup.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Autofac;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Autofac.Extensions.DependencyInjection;
+
+
+namespace TestWindows.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            var containerBuilder = new ContainerBuilder();
+
+            //containerBuilder.RegisterModule<BussinesAutofacModule>();
+            //containerBuilder.RegisterModule(new RepositoryAutofacModule(Configuration));
+
+
+            containerBuilder.Populate(services);
+            var container = containerBuilder.Build();
+            return new AutofacServiceProvider(container);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
